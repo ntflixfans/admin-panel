68d970740cc5976d89c44ac50ff86fe5872dd740
ntflixfans <helloitsme9895@gmail.com>
2025-05-29 18:54:42 +0530

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Welcome to the Admin Panel</h1>
+    </div>
+  );
+};
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
